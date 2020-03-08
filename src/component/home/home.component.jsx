d9db5c400127eb4d585850e0ece6b366363dd9c8
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-03-08 21:53:58 +0000

    get data from api

--- src/component/home/home.component.jsx
+++ src/component/home/home.component.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import { Hero } from "../hero";
+import axios from "axios";
 
 const Home = () => {
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("http://localhost:4455/courseList");
+      const { data: linkList } = await axios.get(
+        "http://localhost:4455/linkList"
+      );
+      const typeList = [
+        "All Courses",
+        ...new Set(data.map(({ type }) => type))
+      ];
+      console.log(data, typeList, linkList);
+    })();
+  }, []);
+
   return (
     <div>
       <Hero />
