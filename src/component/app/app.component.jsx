6cc184e62b66cd6f0778363759db53ae9ae621c5
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-03-08 18:16:59 +0000

    basic navigation using Router

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Router } from "@reach/router";
+
 import { Header } from "../header";
 import "../../index.css";
+import { Home } from "../home";
+import { SignIn } from "../sign-in";
+import { LogIn } from "../log-in";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Router>
+        <Home path="/" />
+        <SignIn path="/sign-in" />
+        <LogIn path="/log-in" />
+      </Router>
     </div>
   );
 };
